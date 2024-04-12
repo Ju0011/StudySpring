87f75ef20c21ac6f7a796deda5be8d27590420f5
Ju0011 <yhs07240@naver.com>
2024-04-12 09:51:21 +0900

    다른 login

--- src/main/java/com/example/firstproject/controller/KakaoLoginController.java
+++ src/main/java/com/example/firstproject/controller/KakaoLoginController.java
@@ -2,6 +2,7 @@ package com.example.firstproject.controller;
 
 import com.example.firstproject.service.KakaoLoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 @Controller
 public class KakaoLoginController {
+    @Autowired
     private final KakaoLoginService kakaoLoginService;
     
     @GetMapping("/login")
     public String login(Model model){
         model.addAttribute("kakaoUrl", kakaoLoginService.getKakaoLogin());
-        return "login"; //View 이름
+        return "login/login"; //View 이름
     }
     
 }
